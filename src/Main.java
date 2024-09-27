a2bc512f7a75a76d21a9bd24c19507eb19d1c4f7
alkaz07 <alkaz07@yandex.ru>
2024-09-27 11:28:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //прочитать строки из файла в список строк
+        List<String> txt = loadText("a.txt");
+        System.out.println(txt);
+        //получить список уникальных имен, прочитанный из файла
+        List<String> unique = new ArrayList<>(new HashSet<>(txt));
+        System.out.println(unique);
+        List<String> unique2 = new ArrayList<>(new TreeSet<>(txt));
+        System.out.println(unique2);
+
+        //подсчитать, сколько раз в файле содержится строка "Вася Пупкин"
+        int k = countVasyaPupkin(txt, "Вася Пупкин");
+        System.out.println("k = " + k);
+    }
+
+    private static int countVasyaPupkin(List<String> txt, String x) {
+
+    }
+
+    private static List<String> loadText(String filename) {
+        //создаем пустой список
+        List<String> lines = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File(filename))){ //открываем сканером файл
+            //циклом вычитываем строки и добавляем в список
+            while (scanner.hasNext()) {
+                String s = scanner.nextLine();
+                lines.add(s);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("файл не открылся");
+            System.out.println(e.getMessage());
+        }
+
+        return lines;
+    }
+}
